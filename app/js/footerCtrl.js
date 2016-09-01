c84ff10bfbe169ec378992a7457516c7a046f044
Emil Westin <emiwes@kth.se>
2016-09-01 09:14:30 +0200

    small fixes.

--- app/js/footerCtrl.js
+++ app/js/footerCtrl.js
@@ -2,7 +2,6 @@ MBDApp.controller('FooterCtrl', function($scope, MBDModel) {
     console.log('FOOTER controller is instantiated!');
 
     $scope.getCompanies = function(){
-        console.log(MBDModel.getCompanies());
         return MBDModel.getCompanies();
     };
 });
