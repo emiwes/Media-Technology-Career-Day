e31a78d69ad49ab431d273b4316f6c191183af84
Emil Westin <emiwes@kth.se>
2016-10-23 14:30:36 +0200

    added "closest event" in schedule.

--- app/js/scheduleCtrl.js
+++ app/js/scheduleCtrl.js
@@ -1,5 +1,31 @@
 MBDApp.controller('ScheduleCtrl', function($scope, MBDModel) {
+    var schedule = MBDModel.getSchedule();
+
+
     $scope.getSchedule = function(){
-        return MBDModel.getSchedule();
+        return schedule;
     };
+
+    $scope.getNearestEvent = function(){
+        var closestEvent = 0;
+        var today = new Date();
+
+        for (var i = 0; i < schedule.length; i++) {
+            var eventDay = schedule[i].day;
+            var startTime = schedule[i].duration.slice(0,5);
+
+            if( eventDay.indexOf("Okt") !== -1 ){
+                eventDay = eventDay.replace("Okt", "Oct");
+            }
+            var eventDate = Date.parse(eventDay + ', 2016 ' + startTime + ':00');
+
+            if(eventDate > today){
+                $scope.closestEvent = schedule[i];
+                break;
+            }
+        }
+    };
+
+    $scope.getNearestEvent();
+
 });
